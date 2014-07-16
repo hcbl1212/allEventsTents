9d00424eb3b8063331f0b70249d5f889538207bb
Brian <bsludwig@svsu.edu>
2014-07-16 18:33:07 -0400

    Changes requested by the sites owners, mostly css styling

--- app/assets/javascripts/tents.js
+++ app/assets/javascripts/tents.js
@@ -3,5 +3,18 @@ $(document).ready(function(){
    $( "#accordion" ).accordion({
       heightStyle: "content"
     });
-   $(".gallery").colorbox();
+   $(".gallery-1").colorbox({rel: 'group1',maxWidth:'556px',maxHeight:'417px', title:"Tents" });
+   $(".gallery-2").colorbox({rel: 'group2',maxWidth:'556px',maxHeight:'417px', title:"Tents" });
+   $(".gallery-3").colorbox({rel: 'group3',maxWidth:'556px',maxHeight:'417px', title:"Tents" });
+   $(".gallery-4").colorbox({rel: 'group4',maxWidth:'556px',maxHeight:'417px', title:"Tents" });
+   $(".gallery-5").colorbox({rel: 'group5',maxWidth:'556px',maxHeight:'417px', title:"Tents" });
+   $(".gallery-6").colorbox({rel: 'group6',maxWidth:'556px',maxHeight:'417px', title:"Tents" });
+   $(".gallery-7").colorbox({rel: 'group7',maxWidth:'556px',maxHeight:'417px', title:"Tents" });
+   $(".gallery-8").colorbox({rel: 'group8',maxWidth:'556px',maxHeight:'417px', title:"Tents" });
+   $(".gallery-9").colorbox({rel: 'group9',maxWidth:'556px',maxHeight:'417px', title:"Tents" });
+   $(".gallery-10").colorbox({rel: 'group10',maxWidth:'556px',maxHeight:'417px', title:"Tents" });
+   $(".gallery-11").colorbox({rel: 'group11',maxWidth:'556px',maxHeight:'417px', title:"Tents" });
+   $(document).tooltip();
+   $("#cboxCurrent").remove();
+   //maxWidth, maxHeight, innerWidth, innerHeight
 });

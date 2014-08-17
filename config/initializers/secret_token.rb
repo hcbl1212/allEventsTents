14d6dc490ad23295a6d905332a0bf3c3e319f50e
Richard Wardin <shalmezad@gmail.com>
2014-08-17 14:50:21 -0400

    More work modifying to be compatible with ruby 1.9.3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AllEventsTents::Application.config.secret_token = '93123f5a04aa6537822c0b58b0006b61ab4ad6060b2c261bedb32e4593e5b588f475a5d75deb338bd7c212b25240b8f438b2839d479023cdc447e5339d3c4ae7'
